71342b512ed6a9dca3619a72a933b1695acc1a0d
Sue Rasmussen <cardnoles10@gmail.com>
2016-03-04 11:41:16 -0500

    add ACC link

--- server/config/config.js
+++ server/config/config.js
@@ -6,10 +6,10 @@ module.exports = {
     db: 'mongodb://localhost:27017/matches',
     rootPath: rootPath,
     port: process.env.PORT || 3030
-  }//,
-  //production: {
+  },
+  // production: {
   //   rootPath: rootPath,
   //   db: 'mongodb://??'
   //   port: process.env.PORT || 80
-  // }
+  //  }
 }
